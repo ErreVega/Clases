a959af1489b06c8a054ffecb7fbcb83081e3361d
Erre-Portatil <errevega1991@gmail.com>
2019-03-03 19:40:29 +0100

    9.1

--- Actividad 9.1/src/Main/Main9_1.java
+++ Actividad 9.1/src/Main/Main9_1.java
@@ -11,35 +11,24 @@ public class Main9_1 {
                 numUser = 0,
                 contIntententos = 0;
         do {
-
-
             try {
-                System.out.print("\nIntroduce numero a adivinar entre 1 y 500: ");
-
+                System.out.print("\nIntroduce numero a adivinar entre 0 y 500: ");
                 contIntententos++;
                 numUser = sc.nextInt();
+                while (numUser < 0 || 500 < numUser){
+                    System.out.println("\nNumero introducido fuera de rango.");
+                    contIntententos++;
+                    numUser = sc.nextInt();
+                }
                 if (numUser > numeroAdivinar)
                     System.out.println("\nNumero introducido mayor.\nIntentos: " + contIntententos);
                 else if (numUser < numeroAdivinar)
                     System.out.println("\nNumero introducido menor.\nIntentos: " + contIntententos);
-                else                 System.out.println("\nNumero adivinado\nIntentos: " + contIntententos);
-
-                System.out.println(numeroAdivinar);
-
-
-            } catch (Exception excp) {
+                else System.out.println("\nNumero adivinado\nIntentos: " + contIntententos);
+            } catch (InputMismatchException e) {
                 System.out.println("\nValor no valido.");
+                sc.nextLine();
             }
-
-
         } while (numUser != numeroAdivinar);
-
-
-
-
-
-
-
-
     }
 }
